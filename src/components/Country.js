3515705a597b0c51a5a22aafb968881bd859306b
Mark Mitchell <mark.mitchell@visitscotland.com>
2021-11-18 22:14:09 +0000

    layout tweaks

--- src/components/Country.js
+++ src/components/Country.js
@@ -11,20 +11,31 @@ const Country = props => {
       <h1 className="country__heading">{name}</h1>
 
       <div className="country__details">
+        <div className="country__datum">
 
-        {
-          altSpellings[1] && <h2>Official Name</h2>
-        }
-        {
-          altSpellings[1] !== name ? <p>{altSpellings[1]}</p> :
-            <p>As above</p>
-        }
-        <h2>Native Name</h2>
-        <p>{nativeName}</p>
-        <h2>Japanese Name</h2>
-        <p> {translations.ja}</p>
-        <h2>Population Density </h2>
-        <p>{Math.round( population / area )} per km<sup>2</sup></p>
+          {
+            altSpellings[1] && <h2>Official Name</h2>
+          }
+          {
+            altSpellings[1] !== name ? <p>{altSpellings[1]}</p> :
+              <p>As above</p>
+          }
+        </div>
+        <div className="country__datum">
+
+          <h2>Native Name</h2>
+          <p>{nativeName}</p>
+        </div>
+        <div className="country__datum">
+
+          <h2>Japanese Name</h2>
+          <p> {translations.ja}</p>
+        </div>
+        <div className="country__datum">
+
+          <h2>Population Density </h2>
+          <p>{Math.round( population / area )} per km<sup>2</sup></p>
+        </div>
       </div>
       <img className="country__flag" src={flag} alt={`flag of ${name}`} />
 
